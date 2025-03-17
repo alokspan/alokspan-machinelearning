ce2384f154bb32a5e8b17dd122a9ee95f4d09a67
alokspan <30453486+alokspan@users.noreply.github.com>
2025-03-16 21:43:08 -0500

    3

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,6 @@ st.title('🎈 App Name')
 
 st.write('Hello world!')
 
-pd.read_csv('http://raw.githubusercontent.com/dataprofressor/data/master/penguins_cleaned.csv')
+pd.read_csv('http://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 
 pd
